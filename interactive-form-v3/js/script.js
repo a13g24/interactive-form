40b22c0addbffa2c7c4dd8917e22a917cc66a75c
AnMiG <a13g24@gmail.com>
2022-01-24 21:04:19 -0700

    Finished requirement 5.

--- interactive-form-v3/js/script.js
+++ interactive-form-v3/js/script.js
@@ -1,13 +1,23 @@
+let jobRoleSelector = document.querySelector('select');
 let otherJobRoleInput = document.getElementById('other-job-role');
-otherJobRoleInput.style.display = 'none';
+let colorSelector = document.getElementById('color');
 
+// things to do when page loads
 document.addEventListener('DOMContentLoaded', () => {
+    // TODO - remove this log
     console.log('DOM loaded and parsed');
+
+    // focus cursor in name field
     document.getElementById('name').focus();
+
+    // hide other job field
+    otherJobRoleInput.style.display = 'none';
+
+    // disable color selector by default
+    colorSelector.disabled = true;
 });
 
-// listen for event change on Job Role selector
-let jobRoleSelector = document.querySelector('select');
+// job role selector listener
 jobRoleSelector.addEventListener('change', (e) => {
     let selectedValue = e.target.value;
 
@@ -17,5 +27,43 @@ jobRoleSelector.addEventListener('change', (e) => {
     } else {
         // hide box if other is not selected
         otherJobRoleInput.style.display = 'none';
+    }
+});
+
+
+let designSelector = document.getElementById('design');
+let jsPuns = [];  // holds js puns shirt colors
+let heartJs = []; // holds heart js shirt colors
+let colorSelectorChildren = colorSelector.children;
+
+// separates puns and hearts colors into disparate arrays
+for (let i of colorSelectorChildren) {
+    let attr = i.getAttribute('data-theme');
+
+    // display only js puns colors
+    if (attr === 'js puns') {
+        jsPuns.push(i);
+    } else if (attr === 'heart js') {
+        heartJs.push(i);
+    }
+}
+
+// design selector listener
+designSelector.addEventListener('change', (e) => {
+    // enable color selector
+    colorSelector.disabled = false;
+
+    // hides/unhides certain options in selection menu
+    let hideUnhide = function(boolOne, boolTwo) {
+        jsPuns.forEach( el => el.hidden = boolOne );
+        heartJs.forEach( el => el.hidden = boolTwo );
+    }
+    
+    // display js puns
+    if (e.target.value.toLowerCase() === 'js puns') {
+        hideUnhide(false, true);
+    } else {
+        // display I heart options
+        hideUnhide(true, false);
     }
 });
